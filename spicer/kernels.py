4e1d8bcd05d1e06afb23f93d8624ee8b61b7dc51
K.-Michael Aye <kmichael.aye@gmail.com>
2015-10-20 00:20:42 -0600

    Removing de432s from generic_kernels for now. adding do_download function to enable download of other items. renamed show_loaded to show_loaded_kernels

--- spicer/kernels.py
+++ spicer/kernels.py
@@ -13,29 +13,50 @@ download_root = 'http://naif.jpl.nasa.gov/pub/naif/generic_kernels/'
 
 generic_kernel_list = ['lsk/naif0011.tls',
                        'pck/pck00010.tpc',
-                       'spk/planets/de432s.bsp',
+                       'spk/planets/de421.bsp',
+                       # 'spk/planets/de430.bsp',
                        ]
 generic_kernels = [KERNELROOT.joinpath(i) for i in generic_kernel_list]
 
 
+def do_download(source, target):
+    """Download source url to target path.
+
+    Parameters
+    ----------
+    source: url <str>
+    target: pathlib.Path
+    """
+
+    try:
+        source.parent.mkdir(parents=True)
+    except FileExistsError:
+        pass
+    print('downloading', source, 'to', target)
+    urlretrieve(source, str(target))
+
+
 def download_generic_kernels():
+    "Download all kernels as required by generic_kernel_list."
+
     dl_urls = [download_root + i for i in generic_kernel_list]
     for dl_url, savepath in zip(dl_urls, generic_kernels):
-        try:
-            savepath.parent.mkdir(parents=True)
-        except FileExistsError:
-            pass
-        print('downloading', dl_url, 'to', savepath)
-        urlretrieve(dl_url, str(savepath))
+        do_download(dl_url, savepath)
 
 
 def check_generic_kernels():
+    "Check for existence of generic_kernels and download if not there."
     if not generic_kernels[0].exists():
         print("Generic kernels do not seem to exist. Downloading ...")
         download_generic_kernels()
 
 
 def load_generic_kernels():
+    """Load all kernels in generic_kernels list.
+
+    Calls `check_generic_kernels()` which downloads the kernels if they are
+    not there.
+    """
     check_generic_kernels()
     # pure planetary bodies meta-kernel without spacecraft data
     for kernel in generic_kernels:
@@ -45,3 +66,22 @@ def load_generic_kernels():
 def load_planet_masses_kernel():
     spice.furnsh(os.path.join(KERNELROOT,
                               'pck/de403-masses.tpc'))
+
+
+def show_loaded_kernels():
+    "Print overview of loaded kernels."
+    count = spice.ktotal('all')
+    if count == 0:
+        print("No kernels loaded at this time.")
+    else:
+        print("The loaded files are:\n(paths relative to kernels.KERNELROOT)\n")
+    for which in range(count):
+        print(which)
+        out = spice.kdata(which, 'all', 100, 100, 100)
+        print("Position:", which)
+        p = Path(out[0])
+        print("Path", p.relative_to(KERNELROOT))
+        print("Type:", out[1])
+        print("Source:", out[2])
+        print("Handle:", out[3])
+        print("Found:", out[4])
