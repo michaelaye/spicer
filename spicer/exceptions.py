3e05e0161d013bf2335cfad90cc8cb91ad7c03cb
K.-Michael Aye <kmichael.aye@gmail.com>
2015-10-20 00:21:10 -0600

    first commit

---
+++ spicer/exceptions.py
@@ -0,0 +1,20 @@
+class SpicerError(Exception):
+    pass
+
+
+class SpiceError(SpicerError):
+
+    def __init__(self, function):
+        self.function = function
+
+    def __str__(self):
+        return "SPICE: Calulating {} failed.".format(self.function)
+
+
+class MissingParameterError(SpicerError):
+
+    def __init__(self, txt):
+        self.txt = txt
+
+    def __str__(self):
+        return "Parameter missing: {}".format(self.txt)
