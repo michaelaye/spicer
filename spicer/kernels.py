c9338e2ace5a0e18126376e5b8eff814b2bd23e7
K.-Michael Aye <kmichael.aye@gmail.com>
2018-12-29 02:16:35 -0700

    fix kernel management
    * downloading fixed
    * fixed wrong de-403-masses filename and folder location at NAIF server
    * added mar097.bsp as default kernel to enable Ls calculation.
    * fixed checking for kernels: Now each, not only first.

--- spicer/kernels.py
+++ spicer/kernels.py
@@ -11,15 +11,17 @@ from pathlib import Path
 modpath = Path(os.path.abspath(__file__))
 dir_path = modpath.parent
 
-KERNELROOT = dir_path / 'kernels'
+KERNELROOT = dir_path / "kernels"
 
-download_root = 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/'
+download_root = "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/"
 
-generic_kernel_list = ['lsk/naif0011.tls',
-                       'pck/pck00010.tpc',
-                       'spk/planets/de403-masses.tpc',
-                       'spk/planets/de430.bsp',
-                       ]
+generic_kernel_list = [
+    "lsk/naif0012.tls",
+    "pck/pck00010.tpc",
+    "pck/de-403-masses.tpc",
+    "spk/planets/de430.bsp",
+    "spk/satellites/mar097.bsp",
+]
 generic_kernels = [KERNELROOT.joinpath(i) for i in generic_kernel_list]
 
 
@@ -31,53 +33,59 @@ def do_download(source, target):
     source: url <str>
     target: pathlib.Path
     """
-
     target.parent.mkdir(parents=True, exist_ok=True)
-    print('downloading', source, 'to', target)
+    print("downloading", source, "to", target)
     urlretrieve(source, str(target))
 
 
-def download_generic_kernels():
+def download_generic_kernels(kernel=None):
     "Download all kernels as required by generic_kernel_list."
-
-    dl_urls = [download_root + i for i in generic_kernel_list]
-    for dl_url, savepath in zip(dl_urls, generic_kernels):
-        do_download(dl_url, savepath)
+    if kernel is None:
+        dl_urls = [download_root + i for i in generic_kernel_list]
+        for dl_url, savepath in zip(dl_urls, generic_kernels):
+            do_download(dl_url, savepath)
+    else:
+        dl_url = download_root + str(kernel.relative_to(KERNELROOT))
+        print(f"Downloading {dl_url} into {kernel}")
+        do_download(dl_url, kernel)
 
 
 def check_generic_kernels():
     "Check for existence of generic_kernels and download if not there."
-    if not generic_kernels[0].exists():
-        print("Generic kernels do not seem to exist. Downloading ...")
-        download_generic_kernels()
+    for kernel in generic_kernels:
+        if not kernel.exists():
+            print(f"Cannot find generic kernel {kernel}. Downloading ...")
+            download_generic_kernels(kernel)
+            print("Done.")
 
 
 def load_generic_kernels():
     """Load all kernels in generic_kernels list.
 
+    Loads pure planetary bodies meta-kernel without spacecraft data.
+
     Calls `check_generic_kernels()` which downloads the kernels if they are
     not there.
     """
     check_generic_kernels()
-    # pure planetary bodies meta-kernel without spacecraft data
     for kernel in generic_kernels:
         spice.furnsh(str(kernel))
 
 
 def load_planet_masses_kernel():
-    spice.furnsh(KERNELROOT / 'pck/de403-masses.tpc')
+    spice.furnsh(KERNELROOT / "pck/de-403-masses.tpc")
 
 
 def show_loaded_kernels():
     "Print overview of loaded kernels."
-    count = spice.ktotal('all')
+    count = spice.ktotal("all")
     if count == 0:
         print("No kernels loaded at this time.")
     else:
         print("The loaded files are:\n(paths relative to kernels.KERNELROOT)\n")
     for which in range(count):
         print(which)
-        out = spice.kdata(which, 'all', 100, 100, 100)
+        out = spice.kdata(which, "all", 100, 100, 100)
         print("Position:", which)
         p = Path(out[0])
         print("Path", p.relative_to(KERNELROOT))
