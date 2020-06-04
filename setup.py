8e70d4621ed04b0c2cc3bd574c52c9007f8e303f
K.-Michael Aye <kmichael.aye@gmail.com>
2020-06-04 13:09:14 -0600

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="spicer",
-    version="0.4.2",
+    version="0.4.3",
     description="Library to make SPICE a bit easier",
     long_description=readme + "\n\n",
     # metadata
--- spicer/__init__.py
+++ spicer/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """K.-Michael Aye"""
 __email__ = "kmichael.aye@gmail.com"
-__version__ = "0.4.2"
+__version__ = "__version__ = '0.4.3'"
 
 from .spicer import *  # noqa
